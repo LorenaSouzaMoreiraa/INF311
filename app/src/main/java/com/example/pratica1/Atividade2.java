4f74c14d42727ee41865acbb4eac5ef792f2f280
Lorena Souza Moreira <111444407+LorenaSouzaMoreiraa@users.noreply.github.com>
2025-03-31 23:52:39 -0300

    Pratica 1 - Atividade 1

---
+++ app/src/main/java/com/example/pratica1/Atividade2.java
@@ -0,0 +1,108 @@
+package com.example.pratica1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Atividade2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.atividade1);
+
+        Button btsum1= (Button) findViewById(R.id.sum1);
+        Button btminus1= (Button) findViewById(R.id.minus1);
+        Button btmultiply1= (Button) findViewById(R.id.multiply1);
+        Button btdivide1 = (Button) findViewById(R.id.divide1);
+
+        EditText input1 = (EditText) findViewById(R.id.value1);
+        EditText input2 = (EditText) findViewById(R.id.value2);
+        TextView result = (TextView) findViewById(R.id.result1);
+
+
+        btsum1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // Captura os valores inseridos
+                    double num1 = Double.parseDouble(input1.getText().toString());
+                    double num2 = Double.parseDouble(input2.getText().toString());
+
+                    // Realiza a soma
+                    double calc = num1 + num2;
+                    result.setText("O resultado é: " + calc); // Exibe o resultado
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Atividade2.this, "Por favor, insira números válidos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Ação para o botão de subtração
+        btminus1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    // Captura os valores inseridos
+                    double num1 = Double.parseDouble(input1.getText().toString());
+                    double num2 = Double.parseDouble(input2.getText().toString());
+
+                    // Realiza a subtração
+                    double cal = num1 - num2;
+                    result.setText("O resultado é: "  + cal); // Exibe o resultado
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Atividade2.this, "Por favor, insira números válidos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Ação para o botão de multiplicação
+        btmultiply1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    // Captura os valores inseridos
+                    double num1 = Double.parseDouble(input1.getText().toString());
+                    double num2 = Double.parseDouble(input2.getText().toString());
+
+                    // Realiza a multiplicação
+                    double cal = num1 * num2;
+                    result.setText("O resultado é: "  + cal); // Exibe o resultado
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Atividade2.this, "Por favor, insira números válidos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Ação para o botão de divisão
+        btdivide1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    // Captura os valores inseridos
+                    double num1 = Double.parseDouble(input1.getText().toString());
+                    double num2 = Double.parseDouble(input2.getText().toString());
+
+                    // Verifica se a divisão por zero foi tentada
+                    if (num2 == 0) {
+                        Toast.makeText(Atividade2.this, "Não é possível dividir por ZERO", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Realiza a divisão
+                        double calc = num1 / num2;
+                        result.setText("O resultado é: "  + calc); // Exibe o resultado
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(Atividade2.this, "Por favor, insira números válidos.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish(); // Fecha a atividade e volta para a anterior
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/pratica1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pratica1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final Button bt1= (Button) findViewById(R.id.atividade1);
+        final Button bt2= (Button) findViewById(R.id.atividade2);
+
+
+        bt1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, Atividade1.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
